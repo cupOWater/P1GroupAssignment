07dec2e1ddff7675837748c0446c8543c9199696
Thanh Huynh <something2805@gmail.com>
2021-12-20 22:46:59 +0700

    Add DisplayInterface abstract and methods for Table/Chart classes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello");
+        ArrayList<Integer> data = new ArrayList<>(Arrays.asList(15, 45, 79, 45, 153, 167, 200, 49, 64, 100, 140, 140, 140, 140));
+        DisplayInterface tableDisplay = new Table(data);
+        tableDisplay.displayData();
     }
 }
